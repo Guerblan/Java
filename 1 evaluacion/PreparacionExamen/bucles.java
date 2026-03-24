930270249fca7da96214e01f74ae41ea575ad11c
Guerblan <lusitania02@hotmail.es>
2026-03-24 13:23:58 +0100

    nuevo

--- 1 evaluacion/PreparacionExamen/bucles.java
+++ 1 evaluacion/PreparacionExamen/bucles.java
@@ -120,23 +120,140 @@ class Bucles20Ejercicios {
     }
 
     public static void ejercicio5() {
+        /*
+         * Ejercicio 9 — Tabla múltiple (empieza a complicarse un poco)
+         * 
+         * Haz un programa que:
+         * 
+         * Muestre las tablas de multiplicar del 1 al 5
+         * Cada tabla debe ir separada
+         * 📌 Salida esperada
+         * Tabla del 1
+         * 1 x 1 = 1
+         * 1 x 2 = 2
+         * 1 x 3 = 3
+         * 1 x 4 = 4
+         * 1 x 5 = 5
+         * 1 x 6 = 6
+         * 1 x 7 = 7
+         * 1 x 8 = 8
+         * 1 x 9 = 9
+         * 1 x 10 = 10
+         * 
+         * Tabla del 2
+         * 2 x 1 = 2
+         * 2 x 2 = 4
+         * ...
+         * 2 x 10 = 20
+         * 
+         * Tabla del 3
+         * ...
+         * 
+         * Tabla del 4
+         * ...
+         * 
+         * Tabla del 5
+         * ...
+         */
+        int resultado = 0;
+        for (int i = 1; i <= 5; i++) {
+            System.out.println();
+            System.out.println("Tabla del " + i);
+            for (int j = 1; j <= 5; j++) {
 
+                resultado = i * j;
+
+                System.out.println(i + " X " + j + " = " + resultado);
+            }
+        }
     }
 
     public static void ejercicio6() {
-
+        /*
+         * Haz un programa que:
+         * 
+         * Pida 10 números al usuario
+         * Cuente cuántos son positivos
+         * Al final muestre:
+         * Cantidad de positivos: X
+         */
+        Scanner sc = new Scanner(System.in);
+        int contador = 0;
+        for (int i = 1; i <= 10; i++) {
+            int numero = sc.nextInt();
+            if (numero > 0) {
+                contador++;
+            }
+        }
+        System.out.println(contador);
     }
 
     public static void ejercicio7() {
-
+        /*
+         * Sumar 10 números introducidos por el usuario
+         * 
+         * Haz un programa que:
+         * 
+         * Pida 10 números
+         * Los vaya sumando
+         * Muestre al final:
+         * La suma total es: X
+         */
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            int num = sc.nextInt();
+            sum += num;
+        }
+        System.out.println(sum);
     }
 
     public static void ejercicio8() {
+        /*
+         * Calcular el promedio de 10 números
+         * 
+         * Haz un programa que:
+         * 
+         * Pida 10 números
+         * Los sume
+         * Calcule el promedio
+         * Muestre:
+         * El promedio es: X
+         */
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
 
+        for (int i = 1; i <= 10; i++) {
+            int num = sc.nextInt();
+            sum += num;
+        }
+        double promedio = sum / 10.00;
+        System.out.println(promedio);
     }
 
     public static void ejercicio9() {
-
+        /*
+         * Encontrar el número mayor
+         * 
+         * Este ejercicio es clave. Sale muchas veces en exámenes.
+         * 
+         * 🔴 Enunciado
+         * 
+         * Haz un programa que:
+         * 
+         * Pida 10 números al usuario
+         * Encuentre el número mayor
+         * Muestre:
+         */
+        Scanner sc = new Scanner(System.in);
+        int mayor = 0;
+        for (int i = 1; i <= 10; i++) {
+            int num = sc.nextInt();
+            if (num > mayor) {
+                mayor = num;
+            }
+        }
+        System.out.println("El número mayor es: " + mayor);
     }
 
     public static void ejercicio10() {
@@ -189,7 +306,7 @@ class Bucles20Ejercicios {
         // ejercicio2();
         // ejercicio3();
         // ejercicio4();
-        // ejercicio5();
+        ejercicio5();
         // ejercicio6();
         // ejercicio7();
         // ejercicio8();
